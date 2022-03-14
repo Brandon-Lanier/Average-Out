47f90d4b39aae866ecfcea9108c83e7a6acd7558
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-14 13:31:24 -0500

    Setting up dev environment

---
+++ src/components/PieChart/PieChart.jsx
@@ -0,0 +1,86 @@
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react';
+import './PieChart.css'
+
+
+function PieChart() {
+
+
+    const assets = useSelector(store => store.assets)
+    // let test = [{id: 1, name: "Bitcoin", quantity: "17.8200", symbol: "btc", user_id: 1}, {id: 2, name: "Ethereum", quantity: "13.8200", symbol: "eth", user_id: 1}]
+    let graphData = [];
+
+ 
+   
+ 
+    const assetSplit = () => {
+        for (let coin of assets) {
+            graphData.push(coin.quantity)
+        }
+        return graphData;
+    }
+
+    let graphNames = [];
+
+    let nameSplit = () => {
+        for (let coin of assets) {
+            graphNames.push(coin.name)
+        }
+        return graphNames;
+    }
+
+    return (
+    <div className="pie-container">
+        <Pie
+        data={{
+            labels: nameSplit(),
+            datasets: [
+            {
+              label: 'Asset Distribution',
+              data: assetSplit(),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+          
+        }}
+       
+        options={{
+            plugins: {
+              legend: {
+                  position: 'bottom'
+              },
+            },
+            responsive: true,
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            },
+            maintainAspectRatio: true,
+            title: {
+                display: true,
+                text: 'Asset Distribution'
+            }
+          }}
+        />
+        </div>
+    )
+}
+
+
+export default PieChart;
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from "react-redux"
+import { useState, useEffect } from "react"
+import PieChart from "../PieChart/PieChart.jsx"
+
+
+function Portfolio() {
+
+    return (
+        <div>
+
+            <PieChart />
+        </div>
+
+    )
+}
