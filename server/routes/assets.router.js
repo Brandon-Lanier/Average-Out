4c04b9fc8e19d7eaf472f926c53c1ae5a6a46935
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-14 13:39:35 -0500

    Added route for handling adding assets

--- server/routes/assets.router.js
+++ server/routes/assets.router.js
@@ -20,6 +20,30 @@ router.get('/', (req, res) => {
 }
 })
 
+router.post('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        console.log(req.user.id);
+        const quantity = Number(req.body.quantity)
+        const coinId = req.body.coin.id
+        const qty = quantity.toFixed(4) // Changing to 4 decimals for DB storage
+        const userId = req.user.id
+        const qryTxt = `
+        INSERT INTO "assets" ("coin_id", "quantity", "user_id") 
+        VALUES ($1, $2, $3)
+        ON CONFLICT ("coinId")
+        DO UPDATE SET "quantity" = "assets"."quantity" + $4;`
+
+        pool.query(qryTxt, [coinId, qty, userId, qty])
+            .then(result => {
+                console.log('You added a coin!');
+                res.sendStatus(201)
+            }).catch(err => {
+                console.log('Error in Add Coin POST', err);
+            })
+    } else {
+        res.sendStatus(403);
+    }
+})
 
 
 module.exports = router;
